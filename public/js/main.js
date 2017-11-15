11b59504860593f3fd2e0ac3315e521eb2d7af89
Ashikjs <ashik@netg5.com>
2017-11-15 13:00:48 +0600

    removed all js file

--- public/js/main.js
+++
@@ -1,1 +0,0 @@
-console.log("Main js now runing"),console.log("now ready Bootstrao js for customizetion");
--- src/js/main.js
+++
@@ -1,2 +0,0 @@
-console.log("Main js now runing");
-console.log("now ready Bootstrao js for customizetion");
